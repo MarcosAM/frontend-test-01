6d451ba5746bb443d617fdf83c4287385b9d595a
DESKTOP-K49PGQT\MarcosMourão <marcosarruda.m@gmail.com>
2019-05-25 15:10:21 -0300

    Adicionado componente widget

---
+++ js/components/widget.js
@@ -0,0 +1,21 @@
+function getWidget() {
+    return'<div class="card mt-4">'+
+            '<div class="d-flex justify-content-between card-header">'+
+                '<p class="align-self-center mb-0" >Widget ID</p>'+
+                    '<div class="btn-group">'+
+                        '<button id="moreOptions" class="btn bmd-btn-icon dropdown-toggle"'+
+                        'type="button" data-toggle="dropdown"'+
+                        'aria-haspopup="true" aria-expanded="false">'+
+                            '<i class="material-icons">more_vert</i>'+
+                        '</button>'+
+                        '<div class="dropdown-menu  dropdown-menu-left" aria-labelledby="moreOptions">'+
+                            '<button class="dropdown-item" type="button">Edit</button>'+
+                            '<button class="dropdown-item" type="button">Delete</button>'+
+                        '</div>'+
+                    '</div>'+
+            '</div>'+
+            '<div class="card-body">'+
+                '<canvas id="lineChart"></canvas>'+
+            '</div>'+
+        '</div>'
+}
