f89c27614e8d9b96d31dcf3af5728ccb77c4d89f
Marcos Mourão <marcosarruda.m@gmail.com>
2019-06-12 13:36:53 -0300

    Refactor: Função dropdownOptions é agora responsável pela criação do ícone de opções

--- js/components/widget.js
+++ js/components/widget.js
@@ -12,33 +12,18 @@ const getWidget = ({ id, data, btns }) => {
         const card = document.createElement('div')
         const header = document.createElement('div')
         const headerTitle = document.createElement('p')
-        const btnGroup = document.createElement('div')
-        const btnDropdown = document.createElement('button')
-        const btnDropdownIcon = document.createElement('i')
-        const dropdownMenu = document.createElement('div')
-        const btnEdit = document.createElement('button')
-        const btnDelete = document.createElement('button')
+        const btnGroup = dropdownOptions([{ text: 'Edit', fun: () => editWidget(id) }, { text: 'Delete', fun: () => deleteWidget(id) }])
         const body = document.createElement('div')
 
         card.classList.add('card', 'mt-4')
         header.classList.add('d-flex', 'justify-content-between', 'card-header')
         headerTitle.classList.add('align-self-center', 'mb-0')
-        btnGroup.classList.add('btn-group')
-        btnDropdown.classList.add('btn', 'bmd-btn-icon', 'dropdown-toggle')
-        btnDropdownIcon.classList.add('material-icons')
-        dropdownMenu.classList.add('dropdown-menu', 'dropdown-menu-left')
-        btnEdit.classList.add('dropdown-item')
-        btnDelete.classList.add('dropdown-item')
+
         body.classList.add('card-body')
 
         card.append(header)
         header.append(headerTitle)
         header.append(btnGroup)
-        btnGroup.append(btnDropdown)
-        btnDropdown.append(btnDropdownIcon)
-        btnGroup.append(dropdownMenu)
-        //dropdownMenu.append(btnEdit)
-        //dropdownMenu.append(btnDelete)
         card.append(body)
         body.append(getChart(data))
 
@@ -46,22 +31,41 @@ const getWidget = ({ id, data, btns }) => {
 
         headerTitle.textContent = id
 
-        btnDropdown.id = 'moreOptions'
-        btnDropdown.setAttribute('type', 'button')
-        btnDropdown.setAttribute('data-toggle', 'dropdown')
-        btnDropdown.setAttribute('aria-haspopup', true)
-        btnDropdown.setAttribute('aria-expanded', false)
-
-        btnDropdownIcon.textContent = 'more_vert'
-
-        dropdownMenu.setAttribute('aria-labelledby', 'moreOptions')
-        dropdownMenu.append(dropdownItem({ text: 'Edit', fun: () => editWidget(id) }))
-        dropdownMenu.append(dropdownItem({ text: 'Delete', fun: () => deleteWidget(id) }))
-
         body.classList.add('card-body')
 
         return card
     }
+}
+
+const dropdownOptions = (itens) => {
+    const btnGroup = document.createElement('div')
+    const btnDropdown = document.createElement('button')
+    const btnDropdownIcon = document.createElement('i')
+    const dropdownMenu = document.createElement('div')
+
+    btnGroup.append(btnDropdown)
+    btnDropdown.append(btnDropdownIcon)
+    btnGroup.append(dropdownMenu)
+
+    btnGroup.classList.add('btn-group')
+    btnDropdown.classList.add('btn', 'bmd-btn-icon', 'dropdown-toggle')
+    btnDropdownIcon.classList.add('material-icons')
+    dropdownMenu.classList.add('dropdown-menu', 'dropdown-menu-left')
+
+    btnDropdown.id = 'moreOptions'
+    btnDropdown.setAttribute('type', 'button')
+    btnDropdown.setAttribute('data-toggle', 'dropdown')
+    btnDropdown.setAttribute('aria-haspopup', true)
+    btnDropdown.setAttribute('aria-expanded', false)
+
+    btnDropdownIcon.textContent = 'more_vert'
+
+    dropdownMenu.setAttribute('aria-labelledby', 'moreOptions')
+
+    const dropdownItens = itens.map(item => dropdownItem(item))
+    dropdownMenu.append(...dropdownItens)
+
+    return btnGroup
 }
 
 const dropdownItem = ({ text, fun }) => {
